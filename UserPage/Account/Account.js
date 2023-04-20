06e4cdc3e3573a081edae7803bff197849771b5c
Brahim_Azirar <90454287+BrahimAzirar@users.noreply.github.com>
2023-04-20 12:38:49 +0000

    start to create posts form

--- UserPage/Account/Account.js
+++ UserPage/Account/Account.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
@@ -7,6 +7,8 @@ import './Account.css';
 export default function Account() {
 
     const [Profile, setProfile] = useState('');
+    const [Post, setPost] = useState(null);
+    const Article = useRef();
     const userId = useParams().userId;
 
     useEffect(() => {
@@ -17,8 +19,19 @@ export default function Account() {
         }));
     }, []);
 
+    function SendPost() {
+        if (Article.current.value) {
+            axios.post('http://localhost:3005/Posts', {
+                PostArticle: Article.current.value, PostData: JSON.stringify(Post),
+                Likes: 0, Dislikes: 0, PostDate: new Date(), sender: parseInt(userId)
+            }).then(resp => console.log(resp.data));
+        } else {
+            alert("Write something !")
+        };
+    }
+
   return (
-    <div className='Parent'>
+    <div className='Parent AccountHome'>
         <header className='row align-items-center border p-2'>
             <h4 className='col-4'>CS|Chat System</h4>
             <div className='col-5'>
@@ -37,9 +50,21 @@ export default function Account() {
             </div>
         </header>
         <section className='row SectionHome'>
-            <div className='Menu col-3'></div>
-            <div className='Content col-6'>
-                <div className='createPost'></div>
+            <div className='Menu col-3'>
+                <ul>
+                    <li><a href="#"><i class="bi bi-house"></i> Home</a></li>
+                </ul>
+            </div>
+            <div className='Content col-6 p-3'>
+                <div className='createPost'>
+                    <textarea ref={Article} className="form-control p-2 mb-3" placeholder='Write your article' style={{ maxHeight: "100px", minHeight: "50px" }}></textarea>
+                    <div>
+                        <div>
+                            <input type="file" id='post' onChange={e => setPost(e.target.files[0])} className="form-control" accept='.jpg, .png, .jpeg, .mkv, .mp4' />
+                        </div>
+                        <button className='btn btn-primary py-1 px-2' onClick={SendPost}>Send <i className="bi bi-send"></i></button>
+                    </div>
+                </div>
                 <div className='Posts'></div>
             </div>
             <div className='col-3'></div>
--- UserPage/EnterToAccount/Login.js
+++ UserPage/EnterToAccount/Login.js
@@ -27,14 +27,14 @@ export default function Login() {
       <form className='w-50 border FormStyle py-4 px-5'>
         <h2 className='text-center my-2'>CS | Chat System</h2>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" ref={Email} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+          <label htmlFor="exampleInputEmail1" className="form-label mb-2">Email address</label>
+          <input type="email" ref={Email} className="form-control SelfStyle" id="exampleInputEmail1" aria-describedby="emailHelp"/>
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" ref={Password} className="form-control" id="exampleInputPassword1"/>
+          <label htmlFor="exampleInputPassword1" className="form-label mb-2">Password</label>
+          <input type="password" ref={Password} className="form-control SelfStyle" id="exampleInputPassword1"/>
         </div>
-        <button type="submit" className="btn btn-primary text-end" onClick={EnterToAccount}>Submit</button>
+        <button type="submit" className="btn btn-primary text-end SelfStyle" onClick={EnterToAccount}>Submit</button>
         <p className='mt-2'>Create an account? <a href="/createaccount">Sign Up</a></p>
       </form>
     </div>
